be6214c90a0d5c75054187af412ca67a82927497
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-05-23 19:30:54 +0200

    del 2-3

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -4,6 +4,18 @@ namespace Slutprojekt
 {
     class Program
     {
+        // Vill användaren försöka igen?
+        static string spelaIgen = "Vill du försöka igen? (j/n)";
+
+        // Slumpar fram ett värde
+         static Random slump = new Random();
+
+        // Tryck på Enter för att fortsätta
+        static string fortsättaMedspelet = "Tryck 'Enter' för att fortsätta!";
+
+        // Användaren matar in fel
+        static string förstodEj = "Jag förstod inte riktigt vad du sa, tryck på enter för att skriva igen!";
+
         static void Main(string[] args)
         {
             Titel();
@@ -14,10 +26,10 @@ namespace Slutprojekt
         /// </summary>
         static void Titel()
         {
-            Console.WriteLine("Välkommen till Dr.Lagoos magiska Ö");
-            Console.WriteLine("Tryck 'Enter' för att fortsätta!");
-            Console.ReadLine();
             Console.Clear();
+            Console.WriteLine("Välkommen till Dr.Lagoos magiska Ö");
+            Console.WriteLine(fortsättaMedspelet);
+            Console.ReadLine();
             // Börjar spelet efter titel
             Del1();
         }
@@ -27,9 +39,11 @@ namespace Slutprojekt
         /// </summary>
         static void Del1()
         {
+            // Vad användaren väljer
             string användarensVal;
 
             // Intro till spelet
+            Console.Clear();
             Console.WriteLine("Solen skinner på dig, du vaknar upp i en äng mitt ute i ingenstans.");
             Console.WriteLine("Du känner att det ligger några saker i dina fickor, du lägger ut de framför dig.");
             Console.WriteLine("Du har:");
@@ -45,26 +59,21 @@ namespace Slutprojekt
 
             switch (användarensVal)
             {
+                // Ifall användaren väljer brevet
                 case "1":
                 case "brevet":
-                {
-                    Console.WriteLine("Välkommen till min mystiska Ö!");
-                    Console.WriteLine("Jag hetter Dr.Lagoo och jag äger allt du ser på denna Ö");
-                    Console.WriteLine("Du undrar säkert varför just du är här, sanningen är att jag kidnappa fel person.");
-                    Console.WriteLine("Hur som helst, jag behöver din hjälp med att ta tillbaka en nyckel som Boris har stulit.");
-                    Console.WriteLine("Nyckeln tillhör en kista som är full med guld");
-                    Console.WriteLine("Du måste besegra Boris för att ta tillbaka nyckeln från honom.");
-                    Console.WriteLine("Självklart så kommer jag att ge dig hälften av det som ligger i kistan samt en resa tillbaka hem.");
-                    Console.WriteLine("Jag förväntar mig din hjälp!");
-                    Console.WriteLine("Mvh");
-                    Console.WriteLine("Dr.Lagoo");
-                    Console.ReadLine();
-                    Console.WriteLine("Tryck 'Enter' för att fortsätta!");
-                    Console.ReadLine();
-                    Alternativ1();
-                    break;
-                }
+                    {
+                        BrevetsInnehåll();
+                        Console.ReadLine();
+                        Console.WriteLine("Du vänder på brevet och ser att det står:");
+                        Console.WriteLine("'Kolla på kartan'");
+                        UndersökerKarta();
+                        Console.ReadLine();
+                        HjälpaLagoo();
+                        break;
+                    }
 
+                // Ifall användaren väljer telefonen
                 case "2":
                 case "telefonen":
                     {
@@ -72,41 +81,31 @@ namespace Slutprojekt
                         Console.WriteLine("Det värkar som att den är avstängd, du testar att hålla ner på On/Off knappen och telefonen slås på!");
                         Console.WriteLine("Hurra! Nu kan jag ringa efter hjälp! (tänker du)");
                         Console.WriteLine("Du knappar in siffrorna '112' i telefonen och trycker på call.");
-                        Console.WriteLine("Mobilen sprängs i örat på dig och du dör på plats!");  
+                        Console.WriteLine("Mobilen sprängs i örat på dig och du dör på plats!");
                         Console.ReadLine();
                         Förlust();
                         break;
                     }
 
+                // Ifall användaren väljer kartan
                 case "3":
                 case "kartan":
                     {
-                        Console.WriteLine("Du viker upp kartan för att se vart du befinner dig.");
-                        Console.WriteLine("Det verkar som att du är i hörnet av mappen och måste föja den röda linjen för attta dig fram till en kista.");
-                        Console.WriteLine("Du undersöker fram och baksidan av kartan ch märker att det står något på baksidan.");
+                        UndersökerKarta();
+                        Console.WriteLine("Du undersöker fram och baksidan av kartan och märker att det står något på baksidan.");
                         Console.WriteLine("Det står:");
-                        Console.WriteLine("");
-                        Console.WriteLine("Välkommen till min mystiska Ö!");
-                        Console.WriteLine("Jag hetter Dr.Lagoo och jag äger allt du ser på denna Ö");
-                        Console.WriteLine("Du undrar säkert varför just du är här, sanningen är att jag kidnappa fel person.");
-                        Console.WriteLine("Hur som helst, jag behöver din hjälp med att ta tillbaka en nyckel som Boris har stulit.");
-                        Console.WriteLine("Nyckeln tillhör en kista som är full med guld");
-                        Console.WriteLine("Du måste besegra Boris för att ta tillbaka nyckeln från honom.");
-                        Console.WriteLine("Självklart så kommer jag att ge dig hälften av det som ligger i kistan samt en resa tillbaka hem.");
-                        Console.WriteLine("Jag förväntar mig din hjälp!");
-                        Console.WriteLine("Mvh");
-                        Console.WriteLine("Dr.Lagoo");
+                        Console.WriteLine("'Läs brevet'");
                         Console.ReadLine();
-                        Console.WriteLine("Tryck 'Enter' för att fortsätta!");
+                        Console.WriteLine("Du öppnar kuveret, och det står:");
+                        BrevetsInnehåll();
                         Console.ReadLine();
-                        Alternativ1();
+                        HjälpaLagoo();
                         break;
                     }
 
                 default:
                     {
-                        Console.WriteLine("Mata in ett giltigt val!");
-                        Console.WriteLine("Tryck 'Enter' för att börja om.");
+                        Console.WriteLine(förstodEj);
                         Console.ReadLine();
                         Del1();
                         break;
@@ -117,7 +116,7 @@ namespace Slutprojekt
         /// <summary>
         /// Vill användaren hjälpa Dr.Lagoo?
         /// </summary>
-        static void Alternativ1()
+        static void HjälpaLagoo()
         {
             string hjälpaLagoo = "";
 
@@ -129,7 +128,7 @@ namespace Slutprojekt
                 Console.WriteLine("");
                 Console.WriteLine("Tänker du hjälpa Dr.Lagoo? (j/n)");
                 hjälpaLagoo = Console.ReadLine().ToLower();
-                
+
                 // användaren dras tillbaka till början
                 if (hjälpaLagoo == "ja" || hjälpaLagoo == "j")
                 {
@@ -150,36 +149,100 @@ namespace Slutprojekt
                 // användaren matar in fel svar
                 else
                 {
-                    Console.WriteLine("Jag förstod inte riktigt vad du sa, tryck på enter för att skriva igen!");
+                    Console.WriteLine(förstodEj);
                     Console.ReadLine();
                 }
             }
         }
 
+        /// <summary>
+        /// Boris ställer en fråga till dig
+        /// </summary>
         static void Del2()
         {
             Console.Clear();
-            Console.WriteLine("Du viker ihop papret och stoppar ner den i fickan.");
-            Console.WriteLine("");
+            int ålder;
+
+            Console.WriteLine("Du viker ihop pappret och stoppar ner den i fickan.");
+            Console.WriteLine("Med bestämda steg så går du mot krysset som är ritad på kartan.");
+            Console.WriteLine("När du kommer fram till din destination så ser du Boris sittande på en stol.");
+            Console.WriteLine("Boris ser förvånad över att du kommit såhär långt och istället för att slås mot dig så säger han");
+
+            // Hur gammal är du?
+            Console.WriteLine("'Jag orkar inte slås mot dig just nu, jag tänker istället fråga dig en frågor, svarar du ärligt så ger jag dig nyckeln.'");
+            Console.WriteLine("'Frågan lyder till såhär:'");
+            Console.WriteLine("'Hur gammal är du?'");
+            int.TryParse(Console.ReadLine(), out ålder);
+
+            // Ifall du är mindre än 70
+            while (ålder < 70)
+            {
+                // Ifall du är högre än 0 men mindre än 70
+                if (ålder > 0)
+                {
+                    Console.WriteLine($"Sluta med dina lögner, du ser ut att vara äldre än {ålder} år.");
+
+                    // Fråga om ålder igen
+                    Console.WriteLine("Hur gammal är du igentligen?");
+                    int.TryParse(Console.ReadLine(), out ålder);
+                }
+                else
+                {
+                    // Ifall du mater in något som inte är en siffra
+                    Console.WriteLine("Förstod inte vad du sa.");
+                    Console.WriteLine("Hur gammal är du?");
+                    int.TryParse(Console.ReadLine(), out ålder);
+                }
+            }
+            Console.WriteLine("'Låter rimmligt, här får du nyckeln till kistan'");
+            Console.WriteLine(fortsättaMedspelet);
+            Console.Clear();
+            Del3();
         }
 
         static void Del3()
         {
+            Console.WriteLine("Efter att du fått nyckeln från Boris så bestämmer du dig för att öppna kistan som ligger bakom honom.");
+            Console.WriteLine("Kistan är proppfull med guld och olika smycken.");
+            Console.WriteLine("Innan du hinner ta något så säger Boris: ");
+            Console.WriteLine("'Dr.Lagoo väntar på dig där uppe'");
+            Console.WriteLine("Du ser att det finns trappor som ledder upp mot toppen av ett berg och du bestämmer dig för att gå upp mot Dr.Lagoo.");
+            Console.WriteLine("Lagoo står där uppe med ryggen vänd mot dig, han vekar stirra ut över kusten.");
+            Console.WriteLine("Han märker din närvaro och säger:");
+            Console.WriteLine("'Jag ser att du har fått tag på nyckeln till kistan du håller i.'");
+            Console.WriteLine("'För att ta dig härifrån med min helikopter så måste du svara på en sista fråga'");
+            Console.WriteLine("'Stava ut ditt namn'");
+            SkrivVertikalt(Console.ReadLine());
+            Console.WriteLine("Boris: '...'");
+            Console.ReadLine();
+            Console.WriteLine("Boris: 'Jag tar hälften av det som finns i kistan och du får i gengäld så får du nyckeln till helikoptern'");
+            Console.WriteLine("Du hoppar in i helikoptern och flyger iväg till frihet med en massa guld i baksätet.");
+            Vinst();
+        }
+
+        static void SkrivVertikalt(string meddelande)
+        {
+            Console.Clear();
+            // Loopa igenom textan tecken för tecken
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                Console.WriteLine(meddelande[i]);
+            }
 
         }
 
+
         /// <summary>
-        /// Du förlora spelet, vill du spea igen eller avsluta?
+        /// Du förlora spelet, vill du spela igen eller avsluta?
         /// </summary>
         static void Förlust()
         {
             string försökIgen = "";
 
             // slumpar fram ett citat som ska stå på konsolen när du har förlorat
-            Random rnd = new Random();
             string[] citatFörlust = {"Wow... det var intressant, visste inte man kunde dö så där.",
             "Du försökte åtminstonde", "Du suger, avinstallera programmet"};
-            int slumpmässigNr = rnd.Next(0,3);
+            int slumpmässigNr = slump.Next(0, 3);
             string förlustText = citatFörlust[slumpmässigNr];
 
             // loopa igenom ifall koden ej känner igen det användaren matar in
@@ -188,9 +251,9 @@ namespace Slutprojekt
                 // alternativ när man förlorat
                 Console.Clear();
                 Console.WriteLine(förlustText);
-                Console.WriteLine("Vill du försöka igen? (j/n)");
+                Console.WriteLine(spelaIgen);
                 försökIgen = Console.ReadLine().ToLower();
-                
+
                 // användaren dras tillbaka till början
                 if (försökIgen == "ja" || försökIgen == "j")
                 {
@@ -206,7 +269,7 @@ namespace Slutprojekt
                 // användaren matar in fel svar
                 else
                 {
-                    Console.WriteLine("Jag förstod inte riktigt vad du sa, tryck på enter för att skriva igen!");
+                    Console.WriteLine(förstodEj);
                     Console.ReadLine();
                 }
             }
@@ -224,7 +287,7 @@ namespace Slutprojekt
                 Console.Clear();
                 Console.WriteLine("Jag gratulerar dig, du har lyckats vinna en väldigt kort och dum spel.");
                 Console.WriteLine("Hoppas du hade det roligt.");
-                Console.WriteLine("Vill du spela igen?");
+                Console.WriteLine(spelaIgen);
                 försökIgen = Console.ReadLine().ToLower();
 
                 // användaren dras tillbaka till början
@@ -242,10 +305,40 @@ namespace Slutprojekt
                 // användaren matar in fel svar
                 else
                 {
-                    Console.WriteLine("Jag förstod inte riktigt vad du sa, tryck på enter för att skriva igen!");
+                    Console.WriteLine(förstodEj);
                     Console.ReadLine();
                 }
             }
         }
+
+        /// <summary>
+        /// Du undersöker kartan
+        /// </summary>
+        static void UndersökerKarta()
+        {
+            Console.WriteLine("Du viker upp kartan för att se vart du befinner dig.");
+            Console.WriteLine("Det verkar som att du är i hörnet av mappen och måste följa den röda linjen för att ta dig fram till krysset.");
+        }
+
+
+        /// <summary>
+        /// Text som står i brevet
+        /// </summary>
+        static void BrevetsInnehåll()
+        {
+            Console.WriteLine("Välkommen till min mystiska Ö!");
+            Console.WriteLine("Jag hetter Dr.Lagoo och jag äger allt du ser på denna Ö");
+            Console.WriteLine("Du undrar säkert varför just du är här, sanningen är att jag kidnappa fel person.");
+            Console.WriteLine("Hur som helst, jag behöver din hjälp med att ta tillbaka en nyckel som Boris har stulit.");
+            Console.WriteLine("Nyckeln tillhör en kista som är full med guld");
+            Console.WriteLine("Du måste besegra Boris för att ta tillbaka nyckeln från honom.");
+            Console.WriteLine("Självklart så kommer jag att ge dig hälften av det som ligger i kistan samt en resa tillbaka hem.");
+            Console.WriteLine("Jag förväntar mig din hjälp!");
+            Console.WriteLine("Mvh");
+            Console.WriteLine("Dr.Lagoo");
+            Console.WriteLine(fortsättaMedspelet);
+        }
+
+
     }
 }
